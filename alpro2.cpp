bdc3788f5e5ccf7e922dbc3bb43f59fede6aa300
Zahra Dini <zahradini646@gmail.com>
2025-09-18 10:11:59 +0700

    first commit

---
+++ alpro2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+       //KAMUS
+	   string nama;
+	   
+	   //ALGORITMA
+       cout << "Tuliskan namamu ; " << endl;
+       
+       cin >> nama ;
+       
+       cout << "Namamu adalah ; " << nama << endl;
+       
+	   return 0;
+}
+
